88bc98e39df8371a1eb7a77f280260069f1bbfac
REINALDO DE PAULA SILVEIRA <22323@unifacef.edu.br>
2022-08-27 14:39:08 -0300

    got

--- app2/index.js
+++ app2/index.js
@@ -4,35 +4,34 @@ const app = express();
 const port = process.env.PORT || 3001;
 const nginxUrl = process.env.NGINX_URL || 'http://localhost:80';
 const shippingUrl = `${nginxUrl}/shipping`;
+// START NEW CODE
+const got = require('got');
+// END NEW CODE
 
-async function requestRetry (retryCount = 0, maxRetryCount = 1) {
-  
-  retryCount++;
+// START MODIFIED CODE
+async function requestRetry (maxRetryCount = 1) {
   let response;
-
   try {
-    response = await requestPromise(shippingUrl);
+    response = await got(shippingUrl, { retry: maxRetryCount }).json();
   } catch(err) {
-    if(retryCount <= maxRetryCount) {
-      console.info(`Executando Retry ${retryCount}`);
-      return await requestRetry(retryCount, maxRetryCount);
-    } else {
-      throw err;
-    }
+    console.error('Error to request /shipping => ', err);
+    throw err;
   }
-  return response;
+  return response
 }
+// END MODIFIED CODE
 
 app.get('/get', async (req, res) => {
   try {
     const response = await requestRetry();
-    console.info(`SUCESS RESPONSE => ${JSON.stringify(response)}`);
+    console.info(`response => ${JSON.stringify(response)}`);
     res.send(response);
   } catch (err) {
-    console.error('ERROR RESPONSE => Alguma coisa ta errado')
+    console.error('Alguma coisa ta errado');
     res.status(500).send('Alguma coisa ta errado');
   }
 });
+
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
