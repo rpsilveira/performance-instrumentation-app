5637d201b88049545a78d48a6ccd66174c1ea56b
REINALDO DE PAULA SILVEIRA <22323@unifacef.edu.br>
2022-08-27 14:26:10 -0300

    initial commit

---
+++ app1/k6/script.js
@@ -0,0 +1,19 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+//const HOST_IP = __ENV.HOST_IP
+
+export const options = {
+  stages: [
+    { target: 20, duration: '2m' }
+  ]
+};
+
+export default function () {
+  //const res = http.get(`http://${HOST_IP}:30000/shipping`);
+  const res = http.get(`http://localhost:53886/shipping`);
+
+  const checkRes = check(res, {
+    'status is 200': (r) => r.status === 200
+  });
+}
